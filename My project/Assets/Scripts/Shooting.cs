cefb1e4f4dc7603ba8f61fed804c6a8219ecbef0
Hunter Dermott <hbdermott@gmail.com>
2022-06-26 20:37:42 -0700

    Movement and shooting
    Movement is now in cardinal directions and diagonals
    
    Sprite now flips based on last horizontal key input
    
    Shooting now based on last keyboard input direction
    
    Bullet rotates properly

---
+++ My project/Assets/Scripts/Shooting.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+    public Vector2 bulletDirection;
+    public float bulletSpeed = 60.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        if(horizontal != 0 || vertical != 0)
+            bulletDirection = new Vector2(horizontal, vertical);
+
+        if(Input.GetKeyDown(KeyCode.Space)){
+            GameObject bullet = Instantiate(bulletPrefab) as GameObject;
+            bullet.transform.position = transform.position + new Vector3(bulletDirection.x, bulletDirection.y, 0.0f);
+            bullet.transform.up = new Vector3(bulletDirection.x, bulletDirection.y, 0.0f);
+            bullet.transform.Rotate(0.0f, 0.0f, 90.0f, Space.Self);
+            bullet.GetComponent<Rigidbody2D>().velocity = bulletDirection * bulletSpeed;
+        }
+    }
+}
