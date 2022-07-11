1f52d1b004c3dfa08337b23883cf00c28034520d
thetimgee <tijgee@ucsc.edu>
2022-07-11 02:17:21 -0500

    Adding Main Menu Music and Round Music

--- My project/Assets/Scripts/Managers/AudioManager.cs
+++ My project/Assets/Scripts/Managers/AudioManager.cs
@@ -32,6 +32,11 @@ public class AudioManager : MonoBehaviour
         }
     }
 
+    void Start()
+    {
+        Play("Theme");
+    }
+
     public void Play (string name)
     {
         Sound s = Array.Find(sounds, sound => sound.name == name);
